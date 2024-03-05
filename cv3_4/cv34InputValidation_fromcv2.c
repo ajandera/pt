f8de9256560af667cee3fefd461ee5cee8d0e9c8
Ales Jandera <ales.jandera@gmail.com>
2024-03-05 11:37:18 +0100

    Add example.

---
+++ cv3_4/cv34InputValidation_fromcv2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int number;
+    int number2;
+    char input[50];
+    char input2[50];
+    int validInput = 0;
+    int validInput2 = 0;
+
+    while (!validInput) {
+        printf("Please enter a number: ");
+        fgets(input, sizeof(input), stdin);
+
+        // Convert input to a number
+        if (sscanf(input, "%d", &number) == 1) {
+            validInput = 1; // Input is a valid number
+        } else {
+            printf("Invalid input. Please enter a valid number.\n");
+        }
+    }
+
+    printf("You entered: %d\n", number);
+
+    while (!validInput2) {
+        printf("Please enter a second number: ");
+        fgets(input2, sizeof(input2), stdin);
+
+        // Convert input to a number
+        if (sscanf(input2, "%d", &number2) == 1 && number2 >= 1) {
+            validInput2 = 1; // Input is a valid number
+        } else {
+            printf("Invalid input. Please enter a valid number.\n");
+        }
+    }
+
+
+    printf("The decision of the numbers is %d.\n", number/number2);
+    return 0;
+}
